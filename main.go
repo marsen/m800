18a6459fba8bfc8109c296144c218c6d1d0b4ad1
Marsen <thisismysoul@gmail.com>
2023-01-17 23:40:29 +0800

    connect to db

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
 
 func main() {
+	viper.SetDefault("mongo.url", "mongodb://localhost:27017")
+	// Set client options
+	clientOptions := options.Client().ApplyURI(viper.GetString("mongo.url"))
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	// Get a handle for your collection
+	collection := client.Database(viper.GetString("mongo.database")).Collection("homework")
+	collection.Find(context.TODO(), bson.D{})
+	fmt.Println("Connected homework")
 	fmt.Println("Hello, World!")
 }
