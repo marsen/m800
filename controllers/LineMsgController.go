b531830c00c07cdd24f47099d39ba13d81f004da
Mark Lin <mark.lin@ttfb-lab.com>
2023-01-18 18:42:42 +0800

    add linemsg controller

---
+++ controllers/LineMsgController.go
@@ -0,0 +1,9 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+type LineMsgController struct{}
+
+func NewLineMsgController(g gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"m800/controllers"
 	"m800/db"
 )
 
 func main() {
 	viper.SetDefault("mongo.url", "mongodb://localhost:27017")
 	db.NewMongoImpl(viper.GetString("mongo.url"))
+	g := gin.Default()
+	g.Use(cors.Default())
+	// 創建一個新的 GameController。
+	gc := &controllers.LineMsgController{}
+	game := g.Group("game")
+	game.GET("query", gc.NewGame)
+	g
 	fmt.Println("Hello, World!")
 }
