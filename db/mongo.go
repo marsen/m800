4b43f904af9823cdb26981df51dbd71bce662f93
Marsen <thisismysoul@gmail.com>
2023-01-18 12:33:09 +0800

    create db

---
+++ db/mongo.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+var client mongo.Client
+
+type MongoImpl struct {
+}
+
+func NewMongoImpl(uri string) {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	// Get a handle for your collection
+	collection := client.Database(viper.GetString("mongo.database")).Collection("homework")
+	collection.Find(context.TODO(), bson.D{})
+	fmt.Println("Connected homework")
+}
--- main.go
+++ main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/spf13/viper"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
+	"m800/db"
 )
 
 func main() {
 	viper.SetDefault("mongo.url", "mongodb://localhost:27017")
-	// Set client options
-	clientOptions := options.Client().ApplyURI(viper.GetString("mongo.url"))
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to MongoDB!")
-
-	// Get a handle for your collection
-	collection := client.Database(viper.GetString("mongo.database")).Collection("homework")
-	collection.Find(context.TODO(), bson.D{})
-	fmt.Println("Connected homework")
+	db.NewMongoImpl(viper.GetString("mongo.url"))
 	fmt.Println("Hello, World!")
 }
