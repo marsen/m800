8b9167166492096f0e30a10f4e134ad1191302ae
Marsen <thisismysoul@gmail.com>
2023-01-19 20:27:49 +0800

    read config

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"m800/controllers"
+)
 
 func main() {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.ReadInConfig()
+	viper.SetDefault("mongo.url", "mongodb://localhost:27017")
+	//db.NewMongoImpl(viper.GetString("mongo.url"))
 	fmt.Println("Hello, World!")
 }
